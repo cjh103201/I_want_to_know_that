da4fb531cef412c8a2732a92be66e02815a7c813
hoon95 <66771509+hoon95@users.noreply.github.com>
2020-06-12 13:36:02 +0900

    Update KBReport_crawling.py

--- KBReport_crawling.py
+++ KBReport_crawling.py
@@ -1,4 +1,3 @@
-run profile1
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
@@ -8,9 +7,10 @@ driver = webdriver.Chrome('chromedriver.exe')
 name=[]    # 이름
 birth=[]   # 생년월일
 height_weight=[]  # 키/몸무게
+i_list = [840,841,842,859,868,869,882,883,889,893,909,913,915,916,917,921,922,923,924,925,926,927,942,943,951,957,961,1004,1036,1054,1057,1335,1373]     # 오류번호 리스트(제외대상)
 
 for i in range(1,1790):
-    try:
+    if i not in i_list:
         url=f'http://www.kbreport.com/player/detail/{i}'
         driver.get(url)
         driver.implicitly_wait(5)
@@ -25,5 +25,3 @@ for i in range(1,1790):
     
         height_weight.append(soup.select(".player-info-3")[0].get_text())     # 키/몸무게
         print(soup.select(".player-info-3")[0].get_text())
-    except:
-        i+=1
