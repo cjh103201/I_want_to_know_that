69cde7f703f4913be7c505dc7c67d5d4ca1fe0f9
jeehyun <cjh103201@naver.com>
2020-06-15 09:25:29 +0900

    날씨 데이터 추가 및 날씨 데이터 1개로 합치는 코드+파일 추가

---
+++ Data_Preprocessing/weather_data_year_merge.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#############
+# by Jeehyun
+# 01 ~ 19년까지 경기가 있던 날 / 지역의 날씨 데이터를 하나의 csv 파일로 합침
+# (4월1일부터 11월30일까지의 데이터)
+#############
+
+import pandas as pd
+from pandas import DataFrame
+
+w_data = pd.read_csv("../Data/weather/2001.csv",  encoding='cp949')
+
+for i in range(2002, 2020):
+    filename = "../Data/weather/" + str(i) + ".csv"
+    tmp = pd.read_csv(filename, encoding='cp949')
+    w_data.append(tmp)
+
+w_data.to_csv("../Data/weather.csv")
+
+## 이후, 일별 경기 데이터와 합칠 예정
