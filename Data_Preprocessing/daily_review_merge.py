3ab96df762cdc6b29bc4dfeca60b04bd152b26e3
jeehyun <cjh103201@naver.com>
2020-06-19 12:31:23 +0900

    일일 선수별 경기 세부 내역 크롤링 데이터 월별 -> 전체 하나로 통합 및 폴더 정리

--- Data_Preprocessing/daily_review_merge.py
+++ Data_Preprocessing/daily_review_merge.py
@@ -5,9 +5,16 @@
 # 월별로 나눠진 daily_review 데이터 merge
 #############
 import pandas as pd
+from pandas import DataFrame
+
+df_total_daily_review = DataFrame()
 
 for y in range(2001, 2020):
     for m in range(3, 11):
         filename = "daily_review_" + str(y) + "_" + str(m) + ".csv"
-        df_month  = pd.read_csv("../Data/"+filename)
-        print(df_month)
+        df_month  = pd.read_csv("../Data/Temp_Data/daily_reviews/"+filename)
+        if len(df_month) > 0:
+            df_total_daily_review = df_total_daily_review.append(df_month, ignore_index=True)
+
+df_total_daily_review.to_csv("../Data/Final_Data/total_daily_data.csv")
+
